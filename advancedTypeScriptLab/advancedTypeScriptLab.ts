4cfec39c3a687146fa56cd2cc7c07ddeaf57e800
Michael Smith <mikesmithnova@gmail.com>
2018-02-09 01:06:19 -0400

    Module 4 Lab 02 Part 1 Complete

--- advancedTypeScriptLab/advancedTypeScriptLab.ts
+++ advancedTypeScriptLab/advancedTypeScriptLab.ts
@@ -43,3 +43,63 @@ function standardizeElements(elemArray: Array<any>) : Array<HTMLElement> {
  }
 
  let standardElements = standardizeElements(elementArray);
+
+ class Rotater {
+    rotate (elem: HTMLElement) {
+        elem.style.transform = "rotate(-315deg)"
+    }
+    rotateBack (elem: HTMLElement) {
+        elem.style.transform = ""
+    }
+}
+
+class Mover {
+    move (elem: HTMLElement) {
+        elem.style.transform = "translateX(50px)"
+    }
+    moveBack (elem: HTMLElement) {
+        elem.style.transform = ""
+    }
+}
+
+class movingElement implements Rotater, Mover {
+    rotate: (elem: HTMLElement) => any
+    move: (elem: HTMLElement) => any
+    moveBack: (elem: HTMLElement) => any
+    rotateBack: (elem: HTMLElement) => any
+    element: HTMLElement
+    constructor(elem: HTMLElement) {
+        elem.onmousedown = () => {
+            this.move(elem);
+        }
+        elem.onmouseup = () => {
+            this.moveBack(elem);
+        }
+        elem.onmouseover = () => {
+            this.rotate(elem);
+        }
+        elem.onmouseout = () => {
+            this.rotateBack(elem);
+        }
+        this.element = elem;
+    }
+}
+
+function applyMixins(derivedClass: any, baseClasses: any[]) {
+    baseClasses.forEach(baseClass => {
+        Object.getOwnPropertyNames(baseClass.prototype).forEach(name => {
+            derivedClass.prototype[name] = baseClass.prototype[name];
+        });
+    });
+}
+applyMixins(movingElement, [Mover, Rotater])
+
+for (let elem of standardElements) {
+    elem.style.width = "60px"
+    elem.style.height = "60px"
+    elem.style.backgroundColor = "green";
+    elem.style.margin = "5px";
+    let elemClass = new movingElement(elem);
+    document.body.appendChild(elemClass.element);
+}
+
